606ce820f7e304b67a04afae5e68490badb297a3
Stephen Ulibarri <druidmech@gmail.com>
2025-09-02 15:36:01 -0700

    Character Classes

---
+++ Source/CrashCourse/Private/Characters/CC_BaseCharacter.cpp
@@ -0,0 +1,15 @@
+// Copyright Druid Mechanics
+
+
+#include "CrashCourse/Public/Characters/CC_BaseCharacter.h"
+
+
+ACC_BaseCharacter::ACC_BaseCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Tick and refresh bone transforms whether rendered or not - for bone updates on a dedicated server
+	GetMesh()->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::AlwaysTickPoseAndRefreshBones;
+}
+
+
