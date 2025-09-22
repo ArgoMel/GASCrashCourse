ffecad2a2fed2655efc40e43542a905c2194ede8
Stephen Ulibarri <druidmech@gmail.com>
2025-09-22 16:48:48 -0600

    Knockback

--- Source/CrashCourse/Public/Utils/CC_BlueprintLibrary.h
+++ Source/CrashCourse/Public/Utils/CC_BlueprintLibrary.h
@@ -50,4 +50,6 @@ public:
 
 	static void DrawHitBoxOverlapDebugs(const UObject* WorldContextObject, const TArray<FOverlapResult>& OverlapResults, const FVector& HitBoxLocation, float HitBoxRadius);
 
+	UFUNCTION(BlueprintCallable, Category = "Crash|Abilities")
+	static TArray<AActor*> ApplyKnockback(AActor* AvatarActor, const TArray<AActor*>& HitActors, float InnerRadius, float OuterRadius, float LaunchForceMagnitude, float RotationAngle = 45.f, bool bDrawDebugs = false);
 };
